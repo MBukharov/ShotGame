dc8a5b5e46f208d4ce93942bdde9af8b564a5da4
MBukharov <mikhail.bukharov@gmail.com>
2024-09-07 18:48:13 +0300

    basic game ready

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import time
 
 pygame.init()
 SCREEN_WIDTH = 800
@@ -13,6 +14,11 @@ icon = pygame.image.load("img/cartoon-weapon.png")
 pygame.display.set_icon(icon)
 
 target_img = pygame.image.load("img/target.png")
+target_width = 80
+target_height = 80
+
+target_x = random.randint(0, SCREEN_WIDTH-target_width)
+target_y = random.randint(0, SCREEN_HEIGTH-target_height)
 
 count = 0
 complexity = 1
@@ -20,6 +26,21 @@ complexity = 1
 running = True
 
 while running:
-    pass
+    screen.fill(screen_color)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
+                target_x = random.randint(0, SCREEN_WIDTH - target_width)
+                target_y = random.randint(0, SCREEN_HEIGTH - target_height)
+                count += 1
+    font = pygame.font.SysFont('couriernew', 20)
+    text = font.render("Счет: "+str(count), True, "black")
+    screen.blit(text, (10, 10))
+    screen.blit(target_img, (target_x, target_y))
+    pygame.display.update()
+
 
 pygame.quit()
